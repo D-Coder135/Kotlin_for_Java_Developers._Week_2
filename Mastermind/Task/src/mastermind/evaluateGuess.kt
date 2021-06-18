239b884f772b355eab8f7ffa5107047e05f968df
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-18 23:37:07 +0530

    add a if() block along with a condition inside the for loop body

--- Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -2,10 +2,11 @@ package mastermind
 
 data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
-    fun evaluateGuess(secret: String, guess: String): Evaluation {
-        var rightPosition = 0
-        var wrongPosition = 0
-        for (index in guess.indices) {
-
+fun evaluateGuess(secret: String, guess: String): Evaluation {
+    var rightPosition = 0
+    var wrongPosition = 0
+    for (index in guess.indices) {
+        if (guess[index] == secret[index]) {
         }
     }
+}
