1393089b3fb6172643076645ba7d5663cfed0e81
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-18 23:32:44 +0530

    initialize a var field name wrongPosition inside the function evaluateGuess() body

--- Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -4,4 +4,6 @@ data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
     fun evaluateGuess(secret: String, guess: String): Evaluation {
         var rightPosition = 0
+        var wrongPosition = 0
+
     }
