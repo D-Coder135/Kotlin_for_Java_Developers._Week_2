353c330d511d79af56ed9938fb2e41351e4e9485
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-18 23:33:33 +0530

    add a for() loop along with a condition inside the method body evaluateGuess()

--- Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -5,5 +5,7 @@ data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
     fun evaluateGuess(secret: String, guess: String): Evaluation {
         var rightPosition = 0
         var wrongPosition = 0
+        for (index in guess.indices) {
 
+        }
     }
