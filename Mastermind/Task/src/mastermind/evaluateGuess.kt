cac5743332f340b5adf48b18daceab08284db6fc
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-18 22:43:38 +0530

    remove the TODO from the body of the method evaluateGuess()

--- Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -1,7 +1,7 @@
 package mastermind
 
-data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
+data class Evaluation(val rightPosition: Int, val wrongPosition: Int) {
 
 fun evaluateGuess(secret: String, guess: String): Evaluation {
-    TODO()
+
 }
